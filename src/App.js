7c0267aa19abe99c4f8d6309243be502ce2f612e
Dan Seagren <seagrendaniel562@gmail.com>
2020-08-24 11:36:07 -0700

    add homepage outline and scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import HomePage from './Components/HomePage'
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <HomePage />
   );
 }
 
