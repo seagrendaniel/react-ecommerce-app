706d578dc109964bdfde25412ca22f822d3c6973
Dan Seagren <seagrendaniel562@gmail.com>
2020-08-24 13:59:09 -0700

    add ShopPage and render items to CollectionPreview component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom'
-import HomePage from './Pages/HomePage'
+import HomePage from './Pages/HomePage/HomePage'
+import ShopPage from './Pages/ShopPage/ShopPage'
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} ></Route>
-        <Route path='/hats' component={HatsPage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
       </Switch>
     </div>
   );
