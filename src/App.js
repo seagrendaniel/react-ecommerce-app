ff1c083082387c78738cfe2909a064a41b5eb7b1
Dan Seagren <seagrendaniel562@gmail.com>
2020-08-25 17:14:30 -0700

    finished firebase integration and create custom button styling for signin/signout

--- src/App.js
+++ src/App.js
@@ -17,17 +17,22 @@ class App extends React.Component {
     }
   }
 
+  unsubscribeFromAuth = null
+
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({ currentUser: user })
     })
   }
 
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
 
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser}/>
         <Switch>
           <Route exact path='/' component={HomePage} ></Route>
           <Route path='/shop' component={ShopPage}></Route>
