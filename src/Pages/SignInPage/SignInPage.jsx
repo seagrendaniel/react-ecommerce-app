affdb9f3bd1409231cc44ba752ffc5b428386711
Dan Seagren <seagrendaniel562@gmail.com>
2020-08-26 11:41:36 -0700

    add email/password signup component

--- src/Pages/SignInPage/SignInPage.jsx
+++ src/Pages/SignInPage/SignInPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SignIn from '../../Components/SignIn/SignIn'
+import SignUp from '../../Components/SignUp/SignUp'
 
 import './SignInPage.scss'
 
 const SignInPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 )
 
