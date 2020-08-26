0c9ef262f87f87923548da2d2bdd3cb6f260dd73
Dan Seagren <seagrendaniel562@gmail.com>
2020-08-26 10:59:33 -0700

    add user to firestore and track user status in App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './Pages/HomePage/HomePage'
 import ShopPage from './Pages/ShopPage/ShopPage'
 import SignInPage from './Pages/SignInPage/SignInPage'
 import Header from './Components/Header/Header'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 import './App.css';
 
@@ -20,8 +20,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+        
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      } else {
+        this.setState({currentUser: userAuth})
+      }
     })
   }
 
