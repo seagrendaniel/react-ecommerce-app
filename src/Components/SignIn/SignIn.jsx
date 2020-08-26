c66dd743eb5f68f729a31e0406ab09d10b93f2e8
Dan Seagren <seagrendaniel562@gmail.com>
2020-08-26 13:34:20 -0700

    add signin with email and passwrd function

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import FormInput from '../FormInput/FormInput'
 import CustomButton from '../CustomButton/CustomButton'
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 import './SignIn.scss'
 
@@ -14,12 +14,15 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault()
-    this.setState({
-      email: '',
-      password: ''
-    })
+    const { email, password } = this.state
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({email: '', password: ''})
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   handleChange = (e) => {
