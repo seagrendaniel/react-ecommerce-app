b168a2b11012b0d84066809e53e3ac9c47f21bc7
Dan Seagren <seagrendaniel562@gmail.com>
2020-08-31 16:14:55 -0700

    add selector for cart dropdown component

--- src/Components/CartDropdown/CartDropdown.jsx
+++ src/Components/CartDropdown/CartDropdown.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import CustomButton from '../CustomButton/CustomButton'
 import CartItem from '../CartItem/CartItem'
+import {selectCartItems} from '../../redux/cart/cart.selectors'
 
 import './CartDropdown.scss'
 
@@ -18,8 +19,8 @@ const CartDropdown = ({cartItems}) => (
   </div>
 )
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown)
