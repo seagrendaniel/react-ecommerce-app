a82e918569bd74e3bbb4db55770639b163c1b88a
Dan Seagren <seagrendaniel562@gmail.com>
2020-08-31 18:15:03 -0700

    add CheckoutItem component and update cart to use selectors from reselect

--- src/Components/CartDropdown/CartDropdown.jsx
+++ src/Components/CartDropdown/CartDropdown.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {createStructuredSelector} from 'reselect'
-import {withRouter} from 'react-router-dom'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { withRouter } from 'react-router-dom'
 
 import CustomButton from '../CustomButton/CustomButton'
 import CartItem from '../CartItem/CartItem'
 
-import {selectCartItems} from '../../redux/cart/cart.selectors'
+import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 import './CartDropdown.scss'
 
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {
-        cartItems.length ? 
-        cartItems.map(cartItem => (
-          <CartItem key={cartItem.key} item={cartItem} />
-        ))
-        : 
-        <span className="empty-message">Your cart is empty</span>
+        cartItems.length ?
+          cartItems.map(cartItem => (
+            <CartItem key={cartItem.key} item={cartItem} />
+          ))
+          :
+          <span className="empty-message">Your cart is empty</span>
       }
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick={() => {
+      history.push('/checkout')
+      dispatch(toggleCartHidden())
+    }}>
+      GO TO CHECKOUT
+    </CustomButton>
   </div>
 )
 
