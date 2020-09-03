17a46256d5a96145208dc9fd39161566f28a6214
Dan Seagren <seagrendaniel562@gmail.com>
2020-09-03 11:40:20 -0700

    add StripeCheckoutButton to CheckoutPage

--- src/Pages/CheckoutPage/CheckoutPage.jsx
+++ src/Pages/CheckoutPage/CheckoutPage.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import CheckoutItem from '../../Components/CheckoutItem/CheckoutItem'
+import StripeCheckoutButton from '../../Components/StripeCheckoutButton/StripeCheckoutButton'
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 
 import './CheckoutPage.scss'
@@ -34,6 +36,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments* 
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 )
 
