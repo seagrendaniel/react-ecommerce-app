fd16cada621cbac2b7a3076a3e0317f51e545264
Dan Seagren <seagrendaniel562@gmail.com>
2020-09-03 10:48:21 -0700

    normalize collections selector

--- src/Components/CollectionsOverview/CollectionsOverview.jsx
+++ src/Components/CollectionsOverview/CollectionsOverview.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {createStructuredSelector} from 'reselect'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import CollectionPreview from '../CollectionPreview/CollectionPreview'
 
-import {selectCollections} from '../../redux/shop/shop.selectors'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors'
 
 import './CollectionsOverview.scss'
 
-const CollectionsOverview = ({collections}) => (
+const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
     {
       collections.map(({ id, ...otherCollectionProps }) => (
@@ -19,7 +19,7 @@ const CollectionsOverview = ({collections}) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview)
