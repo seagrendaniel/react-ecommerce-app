17f0b97ba9fafd7dc30ccdf2414cdde8e81be072
Dan Seagren <seagrendaniel562@gmail.com>
2020-09-10 16:48:54 -0700

    remove unneccesary imports

--- src/Pages/ShopPage/ShopPage.jsx
+++ src/Pages/ShopPage/ShopPage.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-import WithSpinner from '../../Components/WithSpinner/WithSpinner'
 import CollectionsOverviewContainer from '../../Components/CollectionsOverview/CollectionsOverview.container'
 import CollectionPageContainer from '../CollectionPage/CollectionPage.container'
 
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions'
-import { selectIsCollectionFetching, selectIsCollectionLoaded } from '../../redux/shop/shop.selectors'
 
 
 class ShopPage extends React.Component {
